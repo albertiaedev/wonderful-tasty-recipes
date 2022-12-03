7f4f8e4b60df675e25db3bd97e7fa203dd7caee8
Alberto <albertiae.digital@gmail.com>
2022-12-02 21:39:39 -0400

    Decreased the time in delay

--- js/reveal.js
+++ js/reveal.js
@@ -1,4 +1,4 @@
 // scroll reveal
 
-ScrollReveal().reveal('.main', { delay: 2000 });
-ScrollReveal().reveal('.list', { delay: 2000 });
+ScrollReveal().reveal('.main', { delay: 1000 });
+ScrollReveal().reveal('.list', { delay: 1000 });
